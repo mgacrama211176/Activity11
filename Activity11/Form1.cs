317ae9abf62691048d012badae6b458f72387f51
MarlonG <mgacrama_ccs@uspf.edu.ph>
2022-11-19 14:34:17 +0800

    Fixed bugs and made new error trappings

--- Activity11/Form1.cs
+++ Activity11/Form1.cs
@@ -40,38 +40,62 @@ namespace Activity11
             string fname = textBox2.Text.Trim();
             string mname = textBox3.Text.Trim();
             string position = textBox6.Text.Trim();
-            decimal rate = Convert.ToDecimal(textBox5.Text.Trim());
-            int hoursWorked = Convert.ToInt32(textBox4.Text.Trim());
+            decimal rate = 0;
+            int hoursWorked = 0;
             string status = comboBox1.Text.Substring(0, 1);
 
             try
             {
-                using (var connection = new MySqlConnection(connectionString))
+                rate = Convert.ToDecimal(textBox5.Text.Trim());
+                try
                 {
-                    connection.Open();
-                    string sql = "insert into employee(lastname,firstname,middlename,position,rate,hours_worked,status) values ('" + lname + "','" + fname + "','" + mname + "','" + position + "'," + rate + "," + hoursWorked + ",'" + status + "')";
-                    using (MySqlCommand cmd = new MySqlCommand(sql, connection))
+                    hoursWorked = Convert.ToInt32(textBox4.Text.Trim());
+                    try
                     {
-                        cmd.ExecuteNonQuery();
+                        using (var connection = new MySqlConnection(connectionString))
+                        {
+                            connection.Open();
+                            string sql = "insert into employee(lastname,firstname,middlename,position,rate,hours_worked,status) values ('" + lname + "','" + fname + "','" + mname + "','" + position + "'," + rate + "," + hoursWorked + ",'" + status + "')";
+                            using (MySqlCommand cmd = new MySqlCommand(sql, connection))
+                            {
+                                cmd.ExecuteNonQuery();
 
-                        textBox1.Text = "";
-                        textBox2.Text = "";
-                        textBox3.Text = "";
-                        textBox4.Text = "";
-                        textBox5.Text = "";
-                        textBox6.Text = "";
+                                textBox1.Text = "";
+                                textBox2.Text = "";
+                                textBox3.Text = "";
+                                textBox4.Text = "";
+                                textBox5.Text = "";
+                                textBox6.Text = "";
 
-                        textBox1.Focus();
-                        MessageBox.Show("Added to database!");
-                        LoadList();
+                                textBox1.Focus();
+                                MessageBox.Show("Added to database!");
+                                LoadList();
 
+                            }
+                        }
+                    }
+                    catch (Exception err)
+                    {
+                        Console.WriteLine(err.ToString());
                     }
                 }
+                catch (Exception err)
+                {
+                    Console.WriteLine(err);
+                    MessageBox.Show("Error: Hours Worked must be a number!");
+                    textBox4.Text = "";
+                    textBox4.Focus();
+                }
             }
-            catch (Exception err)
+            catch(Exception err)
             {
-                Console.WriteLine(err.ToString());
+                Console.WriteLine(err);
+                MessageBox.Show("Error: rate must be a number!");
+                textBox5.Text = "";
+                textBox5.Focus();
             }
+
+            
         }
 
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
